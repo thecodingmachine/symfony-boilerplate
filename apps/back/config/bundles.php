587e6f7e0383f582be769db5a1858c7aa40ed3bd
Benoit Ngo <b.ngo@thecodingmachine.com>
2023-05-24 11:03:57 +0200

    Adding monolog bundle

--- apps/back/config/bundles.php
+++ apps/back/config/bundles.php
@@ -7,4 +7,5 @@ return [
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
     Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
+    Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
 ];
