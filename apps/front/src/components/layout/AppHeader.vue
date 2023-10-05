54cfe43a5641f9476bc2e61b347358469ad81208
Benoit Ngo <b.ngo@thecodingmachine.com>
2023-10-05 20:39:43 +0200

    fix(primevue): adding primevue in nuxt build

--- apps/front/src/components/layout/AppHeader.vue
+++ apps/front/src/components/layout/AppHeader.vue
@@ -1,12 +1,12 @@
 <template>
-    <div class="grid">
-        <div class="col-2 col-offset-8">
-            <NuxtLink to="/auth/register">Register</NuxtLink>
-        </div>
-        <div class="col-2">
-            <p v-if="username">Welcome {{ username }}</p>
-        </div>
+  <div class="grid">
+    <div class="col-2 col-offset-8">
+      <NuxtLink to="/auth/register">Register</NuxtLink>
     </div>
+    <div class="col-2">
+      <p v-if="username">Welcome {{ username }}</p>
+    </div>
+  </div>
 </template>
 <script setup lang="ts">
 import { useAuthUser } from "~/store/auth";
