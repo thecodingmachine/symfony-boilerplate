c8d0ec4dcab3e3de99cd8b5ec48d12339e6dd602
Benoit Ngo <b.ngo@thecodingmachine.com>
2023-10-09 16:59:11 +0200

    chore(appFetch): upgrade to ofetch.create

--- apps/front/src/plugins/appFetch.ts
+++ apps/front/src/plugins/appFetch.ts
@@ -12,80 +12,22 @@ export default defineNuxtPlugin(() => {
   } = useRequestHeaders(["cookie"]) as {
     [key: string]: string;
   };
-
-  const handleException = (e: any) => {
-    logger.error("An error hapenned during an API call");
-    logger.error(e);
-    // Check if 401 so remove auth info
-    if (e && e.response && e.response.status === 401 && store.isAuthenticated) {
-      logger.error("401 error, removing authentication informations");
-      store.resetAuth();
-    }
-    // HERE the bug
-    const cookies = e.response.headers.get("set-cookie") || "";
-    if (process.server && cookies) {
-      event.res.setHeader("set-cookie", cookies);
-    }
-    throw e;
-  };
-  const fetchRaw = async <T>(
-    request: NitroFetchRequest,
-    opts?: NitroFetchOptions<"json">
-  ) => {
-    const res = await $fetch.raw<T>(request, {
-      headers,
-      baseURL: API_URL,
-      ...opts,
-    });
-
-    const cookies = res.headers.get("set-cookie") || "";
-    if (process.server && cookies) {
-      event.res.setHeader("set-cookie", cookies);
-    }
-    return res;
-  };
-
-  const fetchNative = async <T>(
-    request: NitroFetchRequest,
-    opts?: NitroFetchOptions<"json">
-  ) => {
-    const res = await $fetch.raw<T>(request, {
-      headers,
-      baseURL: API_URL,
-      ...opts,
-    });
-
-    const cookies = res.headers.get("set-cookie") || "";
-    if (process.server && cookies) {
-      event.res.setHeader("set-cookie", cookies);
-    }
-    return res._data;
-  };
-
-  const _appFetchRaw = async <T>(
-    request: NitroFetchRequest,
-    opts?: NitroFetchOptions<"json">
-  ) => {
-    try {
-      return await fetchRaw<T>(request, opts);
-    } catch (e: any) {
-      handleException(e);
-    }
-  };
-  const appFetch = async <T>(
-    request: NitroFetchRequest,
-    opts?: NitroFetchOptions<"json">
-  ) => {
-    try {
-      return await fetchNative<T>(request, opts);
-    } catch (e: any) {
-      handleException(e);
-    }
-  };
-  appFetch.raw = _appFetchRaw;
-  // When you use appFetch.create, you should ensure that you dont need to bridge cookies
-  appFetch.create = $fetch.create;
-
+  const appFetch = $fetch.create({
+    baseURL: API_URL,
+    headers,
+    onResponse(context) {
+      const res = context.response;
+      const cookies = res.headers.get("set-cookie") || "";
+      console.log(res.status);
+      if (res.status === 401 && store.isAuthenticated) {
+        logger.error("401 error, removing authentication informations");
+        store.resetAuth();
+      }
+      if (process.server && cookies) {
+        event.node.res.setHeader("set-cookie", cookies);
+      }
+    },
+  });
   return {
     provide: {
       // // https://nuxt.com/docs/getting-started/data-fetching#example-pass-client-headers-to-the-api
